a73bb23ce52a5848bddfea5b2f545fa870cc1696
Jeerapat Sritawan <talesrunner_dear123@hotmail.com>
2016-10-14 19:31:19 +0700

    add func Captcha

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,3 +1,13 @@
 function createApp(pattern,operator,leftOperand,rightOperand) {
   return new Captcha(pattern,operator,leftOperand,rightOperand);
 }
+
+function Captcha(pattern,operator,leftOperand,rightOperand) {
+  this.generate = function(){
+    let op = new Operator(operator);
+    let lo = new left_operand(pattern,leftOperand);
+    let ro = new right_operand(pattern,rightOperand);
+
+    return lo+' '+op+' '+ro;
+  }
+}
