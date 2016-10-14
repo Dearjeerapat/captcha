061ecda9729b74b8b1d63c14ccfd97a2a8747bf7
Jeerapat Sritawan <talesrunner_dear123@hotmail.com>
2016-10-14 19:47:39 +0700

    add func left_operator

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -19,3 +19,12 @@ function Operator(operator){
     return op[operator - 1];
   }
 }
+
+function left_operand(pattern,leftOperand){
+  this.to_string = function(){
+    let lo1 = ['0','1','2','3','4','5','6','7','8','9'];
+    let lo2 = ['ZERO','ONE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE'];
+
+    return pattern === 1? lo1[leftOperand - 1] : lo2[leftOperand - 1];
+  }
+}
